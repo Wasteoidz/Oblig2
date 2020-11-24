0b0574fd9b6cbfa21b236aa1a58b046b15c3263f
Wasteoidz <71650988+Wasteoidz@users.noreply.github.com>
2020-11-24 23:50:35 +0100

    code done

--- js/Script.js
+++ js/Script.js
@@ -4,38 +4,39 @@ var contentDiv = document.getElementById('content');
 // model
 let numbers = [7, 3, 1, 5, 8];
 let chosenBar; // Variabel for hvilken stolpe som er valgt
-let inputValue; // Variabel for hva som er skrevet i input-feltet
+let inputValue = null; // Variabel for hva som er skrevet i input-feltet
+let errorTekst = '';
 // view
 show();
 function show() {
     let svgInnerHtml = '';
     for (let i = 0; i < numbers.length; i++) {
-        svgInnerHtml += createBar(numbers[i], i + 1);
+        svgInnerHtml += createBar(numbers[i], i + 1, i);
     }
     contentDiv.innerHTML = `
             <svg id="chart" width="500" viewBox="0 0 80 60">
                 ${svgInnerHtml}
             </svg><br/>
-            Valgt stolpe: <i>ingen</i>
+            Valgt stolpe: <i>${chosenBar != null ? chosenBar + 1 : 'ingen stolpe er valgt'}</i>
             <br />
             Verdi:
             <input type="number" min="1" max="10" oninput="inputValue = this.value" />
             <button onclick="addPole()">Legg til stolpe</button>
-            <button disabled>Endre valgt stolpe</button><br />
-            <button disabled>Fjerne valgt stolpe</button><br/>
-            <p id="errorTekst"></p>
+            <button ${chosenBar != null ? '' : 'disabled'} onclick="changePole()">Endre valgt stolpe</button><br />
+            <button ${chosenBar != null ? '' : 'disabled'} onclick="removePole()">Fjerne valgt stolpe</button><br/>
+            <div id="errorTekst">${errorTekst}</div>
             `;
 }
 
-function createBar(number, barNo) {
+function createBar(number, barNo, i) {
     const width = 8;
     const spacing = 2;
     let x = (barNo - 1) * (width + spacing);
     let height = number * 10;
     let y = 60 - height;
     let color = calcColor(1, 10, barNo);
-    return `<rect width="${width}" height="${height}"
-                            x="${x}" y="${y}" fill="${color}"></rect>`;
+    return `<rect onclick="targetPole(${i})" width="${width}" height="${height}"
+                            x="${x}" y="${y}" fill="${color}" stroke =${i == chosenBar ? 'black' : ''} ></rect>`;
 }
 
 function calcColor(min, max, val) {
@@ -45,14 +46,4 @@ function calcColor(min, max, val) {
     return colString;
 }
 
-    // controller (ingenting her ennå)
-
-function addPole() {
-    if (inputValue > 10 || inputValue < 1) {
-        alert(inputValue + " er en ugyldig verdig, venligst velg en verdi fra 1-10.");
-    } else {
-        numbers.push(inputValue);
-    }
-    show();
-
-}
+//controller, se controller.js//
--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,38 @@
 //controller//
+
+function addPole() {
+    if (inputValue > 10 || inputValue < 1) {
+        errorTekst = 'Du har valgt en ugyldig verdig, venligst velg en verdi fra 1-10.';
+    } else {
+        numbers.push(inputValue);
+        errorTekst = '';
+        inputValue = null;
+    }
+    show();
+
+}
+
+function targetPole(pole) {
+    (chosenBar != pole ? chosenBar = pole : chosenBar = null);
+    inputValue = null;
+    show();
+}
+
+function changePole() {
+    if (inputValue > 10 || inputValue < 1) {
+        errorTekst = 'Du har valgt en ugyldig verdig, venligst velg en verdi fra 1-10.';
+    } else {
+        numbers[chosenBar] = inputValue;
+        errorTekst = '';
+        inputValue = null;
+    }
+    show();
+}
+
+
+
+function removePole() {
+    numbers.splice(chosenBar, 1);
+    chosenBar = null;
+    show();
+}
